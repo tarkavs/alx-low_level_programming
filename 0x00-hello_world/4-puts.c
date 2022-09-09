df92e52873cb1a08a116753e1c2120e613123c15
tarkavs <tarkavsunday@outlook.com.com>
2022-09-09 10:36:46 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,16 @@
-#include  <stdio.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
- * Description: C program that prints a function followed by a new line
- * Return: 0 if successful
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-} 
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
