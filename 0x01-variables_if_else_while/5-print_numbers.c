372cd0d12a85d7396871472ab23e47010ba59dfd
tarkavs <tarkavsunday@outlook.com.com>
2022-09-12 02:39:22 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - writing all the number of base ten fro 0 to 10
+ *
+ * Return: 0 always
+ *
+ */
+
+int main(void)
+{
+	int z;
+
+	for (z = 48; z < 58; z++)
+	{
+	putchar(z);
+	}
+	putchar('\n');
+	return (0);
+}
