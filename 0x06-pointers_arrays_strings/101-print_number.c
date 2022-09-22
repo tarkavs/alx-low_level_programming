4b244dc22a4d226d9aab15e3a6cf7e0c87c22dff
tarkavs <tarkavsunday@outlook.com.com>
2022-09-22 04:40:36 -0700

    my commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,44 +1,20 @@
 #include "main.h"
 
 /**
- * rot13 - Encodes a string using rot13.
- * @str: The string to be encoded.
- *
- * Return: A pointer to the encoded string.
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
  */
 
-char *rot13(char *str)
+void print_number(int n)
 {
-	int ind1 = 0, ind2;
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-				'G', 'H', 'I', 'J', 'K', 'L',
-				'M', 'N', 'O', 'P', 'Q', 'R',
-				'S', 'T', 'U', 'V', 'W', 'X',
-				'Y', 'Z', 'a', 'b', 'c', 'd',
-				'e', 'f', 'g', 'h', 'i', 'j',
-				'k', 'l', 'm', 'n', 'o', 'p',
-				'q', 'r', 's', 't', 'u', 'v',
-				'w', 'x', 'y', 'z'};
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-				'T', 'U', 'V', 'W', 'X', 'Y',
-				'Z', 'A', 'B', 'C', 'D', 'E',
-				'F', 'G', 'H', 'I', 'J', 'K',
-				'L', 'M', 'n', 'o', 'p', 'q',
-				'r', 's', 't', 'u', 'v', 'w',
-				'x', 'y', 'z', 'a', 'b', 'c',
-				'd', 'e', 'f', 'g', 'h', 'i',
-				'j', 'k', 'l', 'm'};
-	while (str[ind1])
+	unsigned int num = n;
+
+	if (n < 0)
 	{
-		for (ind2 = 0; ind2 < 52; ind2++)
-		{
-			if (str[ind1] == alphabet[ind2])
-			{
-				str[ind1] = rot13key[ind2];
-				break;
-			}
-		}
-		ind1++;
+		_putchar('-');
+		num = -num;
 	}
-	return (str);
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
 }
