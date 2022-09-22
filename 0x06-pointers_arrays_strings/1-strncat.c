79bb6fdaa4506c4d1eef6d5b25dcc100a9492e98
tarkavs <tarkavsunday@outlook.com.com>
2022-09-21 18:20:50 -0700

    my work

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,30 +1,34 @@
 #include "main.h"
 
 /**
- * _strncat - concatenate two strings using at most n bytes from src
- * @dest: input value
- * @src: input value
- * @n: input value
- *
+ * _strncat - concatenates two strings
+ * @dest: A pointer to a character that will be changed
+ * @src: A pointer to a character that will be changed
+ * @n: value
  * Return: dest
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
-	int a;
-	int b;
+int i, j;
 
-	a = 0;
-	while (dest[a] != '\0')
-	{
-		a++;
-	}
-	b = 0;
-	while (b < n && src[b] != '\0')
-	{
-		dest[a] = src[b];
-		a++;
-		b++;
-	}
-	dest[a] = '\0';
-	return (dest);
+i = 0;
+
+while (dest[i] != '\0')
+{
+i++;
+}
+
+j = 0;
+
+while (j < n && src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+
+dest[i] = '\0';
+
+return (dest);
 }
